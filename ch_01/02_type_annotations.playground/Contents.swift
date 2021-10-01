6269b1d907cec2a943ec05e78241e524e39bf2c0
formegusto <formegusto@gmail.com>
2021-10-01 12:04:11 +0900

    commit

---
+++ ch_01/02_type_annotations.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// Type Annotaion
+// Swift 언어 개념
+// 2. Swift는 기본적으로 타입추론을 한다.
+// 때문에 타입정의를 하고 싶다면 아래와 같이 사용한다.
+
+var str: String = "Hello, playground"
+var myAge: Int = 10
+var screenHeight: Float = 560.10
+
+// Error 가 난다.
+// Swift에서 다른 타입간의 연산은 불가능하다.
+// Type Saft
+// var sum = myAge + screenHeight
+var sum = myAge + Int(screenHeight)
